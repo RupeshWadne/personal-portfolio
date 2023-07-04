554189b7ca483f17102ef6255d818556859995fd
shubham_wadne <kaylekashubham@gmail.com>
2023-07-04 11:56:54 +0530

    update with final resume

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './about.css'
 import AboutImg from "../../assets/shubham.png"
-import CV from "../../assets/NewResumeforjuly.pdf"
+import CV from "../../assets/FinalFinal.pdf"
 import Info from "./Info"
 
 const About = () => {
